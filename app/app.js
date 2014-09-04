513537023c0523c90383c0600b0ee6bfab862465
Abhinav Gujjar <abhinavgujjar@gmail.com>
2014-09-04 16:52:00 +0530

    filters

--- app/app.js
+++ app/app.js
@@ -43,7 +43,7 @@ function hotelsController($scope) {
         "location": "Whitefield",
         "imgUrl": "OqJMuy5.jpg",
         "availableForBook": true,
-        "description": "Irresolutely and oh across wherever oh goodness more wallaby a some much reindeer octopus overcast howled far far honey tamarin a ouch inventoried distinct wombat egregious deceptive conservative alas.",
+        "description": "Artful goodness as depending naively suitably disagreeably more krill alongside wherever a far krill fled irrespective the the began thus desolate that more madly that less that off the dauntlessly this avowed far bravely beside where yikes and drew or opossum shut jeepers doused bowed jeepers impious this loaded deftly walked until that and porpoise tortoise while busted tunefully spread dear one unlocked that assisted much excepting cordially that crud.",
         "area": 20000
     }, {
         "id": "3",
@@ -52,7 +52,7 @@ function hotelsController($scope) {
         "location": "Old Airport Road",
         "imgUrl": "qA4PCDl.jpg",
         "availableForBook": true,
-        "description": "Llama crab credible wow reproachfully brave apart dived rugged this hey luscious obediently pert taped smug crud learned thus arousingly or jeepers as before pill save jeez cunning jeez some pointed near house imprecise chose.",
+        "description": "Visually far vivacious one this swore and wow that opposite more and goldfish coward besides much labrador bashfully one well rang after a and darn wow far browbeat playfully excited less more when rash well radically wildebeest one well a hypnotically vehemently stupid so.",
         "area": 65000
     }, {
         "id": "4",
@@ -61,7 +61,7 @@ function hotelsController($scope) {
         "location": "Whitefield",
         "imgUrl": "qA4PCDl.jpg",
         "availableForBook": true,
-        "description": "Llama crab credible wow reproachfully brave apart dived rugged this hey luscious obediently pert taped smug crud learned thus arousingly or jeepers as before pill save jeez cunning jeez some pointed near house imprecise chose.",
+        "description": "Some radiant resigned however imprecise unthinkingly amongst as but criminally between far less speechlessly mowed yikes sorrowful and hence up jellyfish unreceptive off goodness seal coughed poetic outgrew jeez thus came ferret thus stridently outbid before snuffed dragonfly dispassionately gosh more indescribable kneeled nutria gurgled irrespective sorely dear pointed hardheadedly so gosh from that rode convulsive saluted packed however yikes over more went and barring away egret and partook sober komodo loaded blissfully scorpion upon jeepers stolid close square that far outside breathlessly more thus oh alas nauseatingly as overpaid more unbound wistful warthog shut shamefully seagull squid sourly.",
         "area": 198000
     }];
 
