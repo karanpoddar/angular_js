561c7cc5ab083f7cf54ad5f3eb6c9be69ceffcde
Abhinav Gujjar <abhinavgujjar@gmail.com>
2014-09-05 16:40:11 +0530

    nested forms

--- app/js/controllers/newHotelController.js
+++ app/js/controllers/newHotelController.js
@@ -2,6 +2,14 @@ angular.module('discoHotel').controller('newHotelController',
 
 	function($scope, hotelsProvider, $location) {
 
+		$scope.step = 1;
+		$scope.moveNext = function(){
+			$scope.step++;
+		}
+		$scope.movePrev = function(){
+			$scope.step--;
+		}
+
 		$scope.addHotel = function(form, hotel) {
 			if (form.$invalid) {
 				alert('NOT VALID');
