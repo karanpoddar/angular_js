48b6a3f81cd3af26075287a842a49602e1c60e08
Abhinav Gujjar <abhinavgujjar@gmail.com>
2014-09-08 10:52:30 +0530

    parse api intergration

--- app/js/services/hotelsProvider.js
+++ app/js/services/hotelsProvider.js
@@ -11,13 +11,19 @@ angular.module('discoHotel').factory('hotelsProvider',
 
                 var deferred = $q.defer();
 
-                $http.get('data/hotels.json').success(function(hotels){
+                $http.get('https://api.parse.com/1/classes/Hotel', {
+                    headers: {
+                        'X-Parse-Application-Id': 'VTWSWnJAkMedzSXCQJgsEfKoIIAtnEcpnRuT4uNk',
+                        'X-Parse-REST-API-Key': 'HC3oyBVcUaHvAHs1BRJ8HeuqYtBActgToyeP5CNq',
+                    }
 
-                    $timeout(function(){
+                }).success(function(hotels) {
+
+                    $timeout(function() {
                         deferred.resolve(hotels.results);
                     }, 500);
-                    
-                }).error(function(error){
+
+                }).error(function(error) {
                     //alert(error);
 
                     deferred.reject('could not find hotels');
@@ -26,8 +32,17 @@ angular.module('discoHotel').factory('hotelsProvider',
                 return deferred.promise;
 
             },
-            addHotel : function(hotel){
+            addHotel: function(hotel) {
                 //hotels.push(hotel);
+
+                $http.post('https://api.parse.com/1/classes/Hotel', hotel, {
+                    headers: {
+                        'X-Parse-Application-Id': 'VTWSWnJAkMedzSXCQJgsEfKoIIAtnEcpnRuT4uNk',
+                        'X-Parse-REST-API-Key': 'HC3oyBVcUaHvAHs1BRJ8HeuqYtBActgToyeP5CNq',
+                    }
+
+                });
+
             }
 
         };
